201f67ea0a64357ce8337f31f90d6f8b6f736b4e
LSlayer123 <47830353+LSlayer123@users.noreply.github.com>
2022-04-21 21:56:51 +0100

    Added Path for the Panda to walk

--- Panda-Tests.cxx
+++ Panda-Tests.cxx
@@ -4,10 +4,14 @@
 #include "genericAsyncTask.h"
 #include "asyncTaskManager.h"
 
+#include "cIntervalManager.h"
+#include "cLerpNodePathInterval.h"
+#include "cMetaInterval.h"
+
 PT(AsyncTaskManager) taskMgr = AsyncTaskManager::get_global_ptr();
 PT(ClockObject) globalClock = ClockObject::get_global_clock();
-NodePath camera;
 
+NodePath camera;
 
 AsyncTask::DoneStatus spinCameraTask(GenericAsyncTask *task, void *data) {
 
@@ -55,8 +59,48 @@ int main(int argc, char *argv[]) {
   // Adds (really bad) camera controls with the mouse.
   // window->setup_trackball();
 
+  // Definining intervals for the Panda to walk through in the space.
+  PT(CLerpNodePathInterval) pandaPosInterval1, pandaPosInterval2, pandaHprInterval1, pandaHprInterval2;
+
+  // Setting each interval's values separately (Maybe there's a better way of doing this? - i.e. separate function or something)
+  pandaPosInterval1 = new CLerpNodePathInterval("pandaPosInterval1", 13.0, CLerpInterval::BT_no_blend, true, false, pandaActor, NodePath());
+  pandaPosInterval1->set_start_pos(LPoint3(0, 10, 0));
+  pandaPosInterval1->set_end_pos(LPoint3(0, -10, 0));
+
+  pandaPosInterval2 = new CLerpNodePathInterval("pandaPosInterval2", 13.0, CLerpInterval::BT_no_blend, true, false, pandaActor, NodePath());
+  pandaPosInterval2->set_start_pos(LPoint3(0, 10, 0));
+  pandaPosInterval2->set_end_pos(LPoint3(0, -10, 0));
+
+  pandaHprInterval1 = new CLerpNodePathInterval("pandaHprInterval1", 3.0, CLerpInterval::BT_no_blend, true, false, pandaActor, NodePath());
+  pandaHprInterval1->set_start_hpr(LPoint3(0, 0, 0));
+  pandaHprInterval1->set_end_hpr(LPoint3(180, 0, 0));
+
+  pandaHprInterval2 = new CLerpNodePathInterval("pandaHprInterval2", 3.0, CLerpInterval::BT_no_blend, true, false, pandaActor, NodePath());
+  pandaHprInterval2->set_start_hpr(LPoint3(180, 0, 0));
+  pandaHprInterval2->set_end_hpr(LPoint3(0, 0, 0));
+
+  // Creating the sequence that coordinates the intervals
+  PT(CMetaInterval) pandaPace;
+  pandaPace = new CMetaInterval("pandaPace");
+
+  pandaPace->add_c_interval(pandaPosInterval1, 0, CMetaInterval::RS_previous_end);
+  pandaPace->add_c_interval(pandaPosInterval2, 0, CMetaInterval::RS_previous_end);
+  pandaPace->add_c_interval(pandaHprInterval1, 0, CMetaInterval::RS_previous_end);
+  pandaPace->add_c_interval(pandaHprInterval2, 0, CMetaInterval::RS_previous_end);
+
+  // Looping the sequence
+  pandaPace->loop();
+
+
   // Adds defined camera task from above.
   taskMgr->add(new GenericAsyncTask("Spins the camera", &spinCameraTask, nullptr));
+
+
+  // Alteranate way of performing a task every frame without tasks
+  Thread* current_thread = Thread::get_current_thread();
+  while (framework.do_frame(current_thread)) {
+	  CIntervalManager::get_global_ptr()->step();
+  }
 
 
   // Calls Main loop.
